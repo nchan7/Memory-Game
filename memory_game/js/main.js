234cffd959d7dde26978bdcd0243987c68e37629
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-05-25 15:28:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,6 +9,30 @@ console.log("User flipped " + cardThree);
 */
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!")
+		} else {
+			alert("Sorry, try again.")
+		}
+	}
+}
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+	
+
+/* 
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
@@ -22,6 +46,5 @@ if (cardsInPlay.length === 2) {
 	} else {
 		alert("Sorry, try again.")
 	}
-
-
 }
+*/
